25ec73e97896e260ef8d94129ac0b5c94bd96ea7
sbly314 <sbly314@users.noreply.github.com>
2016-07-23 18:18:36 -0500

    Update Controller.java

--- src/CombinedCode/Controller/Controller.java
+++ src/CombinedCode/Controller/Controller.java
@@ -176,7 +176,7 @@ class clientThread extends Thread {
 	  } else {
 		 if(temp2[0].equals("NONE")){
 		 	System.out.println("\nSearching for all files in category "+temp2[1]+"\n");
-			dbUser.dbParser(dbUser.queryCriteria(temp2[1], null)); // Category-only
+			dbUser.dbParser(dbUser.queryCriteria(null, temp2[1])); // Category-only
 		 }
 		 else{
 		 	System.out.println("\nSearching for "+temp2[0]+" in category "+temp2[1]+"\n");
