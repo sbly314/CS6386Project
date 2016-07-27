a9cc483cdf02a472fc23b2ce248a7b0ee8c82cb9
sayantani1 <sayantani.goswami20@gmail.com>
2016-07-26 23:37:45 -0500

    Update Update.sh

--- src/TSDS/Update.sh
+++ src/TSDS/Update.sh
@@ -11,44 +11,42 @@ echo "--------------------------------------------------------"
 echo " "
 echo "Enter Command:"
 read command
+dir=$(pwd)
+
 
 if [ "$command" = 'VIEW' ];then
 	echo " "
 	echo "---------------------Available Media Files---------------------"
 	echo " "
 	echo "Category: Drama"
-	ls Downloads/Drama -1
-	ls Downloads/Drama -1 > Drama.txt
+	ls $dir/Drama -1
+	ls $dir/Drama -1 > Drama.txt
 	echo " "
 	echo "Category: Fiction"
-	ls Downloads/Fiction -1 > Fiction.txt
-	ls Downloads/Fiction -1
+	ls $dir/Fiction -1 > Fiction.txt
+	ls $dir/Fiction -1
 	echo " "
 	echo "Category: Romance"
-	ls Downloads/Romance -1 > Romance.txt
-	ls Downloads/Romance -1
+	ls $dir/Romance -1 > Romance.txt
+	ls $dir/Romance -1
 	echo " "
 	echo "Category: Horror"
-	ls Downloads/Fiction -1 > Horror.txt
-	ls Downloads/Horror -1
+	ls $dir/Horror -1 > Horror.txt
+	ls $dir/Horror -1
 echo "---------------------------------------------------------------"
 fi
 
 if [ "$command" = 'SUBSCRIBE' ];then
-	 echo " "
-       # ls Add/Drama -1 > Drama.txt
-       # ls Add/Fiction -1 > Fiction.txt
-       # ls Add/Romance -1 > Romance.txt
-       # ls Add/Fiction -1 > Horror.txt
-file=Drama.txt
-#ls Downloads/Drama | wc -l
-cat $file | sed -e "s/#.*//" | sed -e "/^\s*$/d" | (
-    while read line
-    do
-     	java Update $command $line-Drama 
-	echo "$line added successfully"  
-    done
-)
+        ls $dir/Drama -1 > Drama.txt
+        ls $dir/Fiction -1 > Fiction.txt
+        ls $dir/Romance -1 > Romance.txt
+        ls $dir/Horror -1 > Horror.txt
+	java MediaServer > MediaServer.log &
+	mv $dir/Drama/* $dir/ 
+	mv $dir/Fiction/* $dir/
+	mv $dir/Horror/* $dir/
+        mv $dir/Romance/* $dir/
+	echo "Subscribed to Media Server successfully"
 echo "---------------------------------------------------------------"
 fi
 
@@ -60,8 +58,8 @@ if [ "$command" = 'REMOVE' ];then
 	 echo " "
 	 echo "Enter Category:"
          read category
-         java Update $command $mediaName-$category
-	 rm Downloads/$category/$mediaName
+         java Update $command $mediaName-$category > UpdateRemove.log &
+	 rm $dir/$mediaName
          echo "$mediaName removed  successfully"
 echo "---------------------------------------------------------------"
 fi
@@ -73,7 +71,7 @@ if [ "$command" = 'ADD' ];then
          echo " "
          echo "Enter Category:"
          read category
-         java Update $command $mediaName-$category
+         java Update $command $mediaName-$category > UpdateAdd.log &
          echo "$mediaName added successfully to Controller database"
 echo "---------------------------------------------------------------"
 fi
